50b4ca95713e7e7df71fad6673be22f8a564a467
Ignacio Agustin Diaz Neila <ignaciodiazneila@gmail.com>
2025-07-12 22:06:38 -0300

    Fix: import Header.css with correct capitalization for Netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // 👈 Importá Link
 import { FaBars, FaTimes, FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
-import "../styles/header.css";
+import "../styles/Header.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
