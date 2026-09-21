b994ef82626a25b69074e57e4840dbc2a4319c41
Ignacio Agustín Díaz Neila <92888908+IgnacioDN@users.noreply.github.com>
2026-09-20 21:05:05 -0300

    Update estimated year from 2024 to 2025

--- src/components/InformativeSection.jsx
+++ src/components/InformativeSection.jsx
@@ -20,7 +20,7 @@ const InformativeSection = () => {
         <article className="banner-video-one">
           <div className="banner-video-content">
             <div className="video-text">
-              <span className="video-eyebrow">Est. 2024</span>
+              <span className="video-eyebrow">Est. 2025</span>
               <h2>Urban27 is growing fast.</h2>
               <p>Explore with us new opportunities.</p>
               <button className="bannervideo-btn" onClick={handleShopNow}>Shop Now</button>
